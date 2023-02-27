d37ded1e366e1cefab0cb380bd3e10af19dfe466
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-28 00:00:46 +0500

    DATA: Model & Migrate Job

---
+++ CashOverflowUz/Models/jobs/Job.cs
@@ -0,0 +1,18 @@
+﻿//-------------------------------------------------
+// Copyright (c) Coalition OF Good-Hearted Engineers 
+// Developet by CashOverflowUz Team
+//--------------------------------------------------
+
+using System;
+
+namespace CashOverflowUz.Models.job
+{
+    public class Job
+    {
+        public Guid id { get; set; }
+        public string Title { get; set; }
+        public Level  level { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+
+    }
+}
---
+++ CashOverflowUz/Models/jobs/Level.cs
@@ -0,0 +1,15 @@
+﻿//-------------------------------------------------
+// Copyright (c) Coalition OF Good-Hearted Engineers 
+// Developet by CashOverflowUz Team
+//--------------------------------------------------
+
+namespace CashOverflowUz.Models.job
+{
+    public enum  Level
+    {
+        Other,
+        Juniyor,
+        Middle,
+        Senior
+    }
+}
