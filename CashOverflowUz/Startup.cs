030e58d0e84517b457ca2c8f1e65be2ddb77e0de
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-27 17:06:52 +0500

    INFRA: Initialize Api Project

---
+++ CashOverflowUz/Startup.cs
@@ -0,0 +1,56 @@
+//-------------------------------------------------
+// Copyright (c) Coalition OF Good-Hearted Engineers 
+// Developet by CashOverflowUz Team
+//--------------------------------------------------
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CashOverflowUz
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(Action =>
+            {
+                Action.SwaggerDoc(
+                    name:"v1",
+                   info: new OpenApiInfo { Title = "CashOverflowUz", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment enveriment)
+        {
+            if (enveriment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(Action =>
+                Action.SwaggerEndpoint(
+                    url:"/swagger/v1/swagger.json",
+                    name:"CashOverflowUz v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+
+        }
+    }
+}
