7a985cbb6e3bf60b0392d1f79e2657ba760e8bc4
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-02 00:09:58 +0500

    BROKERS: Insert Job

---
+++ CashOverflowUz/Brokers/Storages/IStorageBroker.Job.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CashOverflowUz.Models.job;
+
+namespace CashOverflowUz.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Job> InsertJobAsync(Job job);
+    }
+}
