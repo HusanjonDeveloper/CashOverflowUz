bd4ddb5788047b503ee640b3de2ce266603990fc
Xusanjon Muhammadaliyev <aliakbarov550@gmail.com>
2023-03-08 08:11:53 +0500

    FOUNDATIONS:Retrieve All Language

---
+++ CashOverflowUz.Tests.unit/Servies/Faundetions/Languages/LanguageServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+using System.Linq;
+using CashOverflowUz.Models.Languages;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace CashOverflow.Tests.Unit.Services.Foundations.Languages
+{
+    public partial class LanguageServiceTests
+    {
+        private readonly object languageService;
+
+        [Fact]
+        public void ShouldRetrieveAllLanguages()
+        {
+            // given
+            IQueryable<Language> randomLanguages = CreateRandomLanguages();
+            IQueryable<Language> storageLanguages = randomLanguages;
+            IQueryable<Language> expectedLanguages = storageLanguages.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllLanguages())
+                    .Returns(storageLanguages);
+
+            // when
+            IQueryable<Language> actualLanguages =
+                this.languageService.RetrieveAllLanguages();
+
+            // then
+            actualLanguages.Should().BeEquivalentTo(expectedLanguages);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllLanguages(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+     
+    }
+}
--- CashOverflowUz/Brokers/Storages/StorageBroker.Language.cs
+++ CashOverflowUz/Brokers/Storages/StorageBroker.Language.cs
@@ -1,20 +1,34 @@
-﻿//-------------------------------------------------
-// Copyright (c) Coalition OF Good-Hearted Engineers 
-// Developet by CashOverflowUz Team
-//--------------------------------------------------
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using CashOverflow.Models.Languages;
 using CashOverflowUz.Models.Languages;
 using Microsoft.EntityFrameworkCore;
 
-namespace CashOverflowUz.Brokers.Storages
+namespace CashOverflow.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Language> Languages { get; set; }
 
-       public async ValueTask<Language> InsertLanguageAsync(Language language) =>
-            await InsertAsync(language);
+        public async ValueTask<Language> InsertLanguageAsync(Language language) =>
+            await this.InsertAsync(language);
 
+        public IQueryable<Language> SelectAllLanguages() =>
+            SelectAll<Language>();
+
+        public async ValueTask<Language> SelectLanguageByIdAsync(Guid languageId) =>
+            await SelectAsync<Language>(languageId);
+
+        public async ValueTask<Language> UpdateLanguageAsync(Language language) =>
+            await UpdateAsync(language);
+
+        public async ValueTask<Language> DeleteLanguageAsync(Language language) =>
+            await DeleteAsync<Language>(language);
     }
 }
