d9b6d95147716d2397dcb57ff71bf1384370693b
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-09 23:11:51 +0500

    ShouldThrowValidationExceptionOnAddIFInputIsNullAndLogItAsync->PAss

--- CashOverflowUz/Services/Foundetions/Locations/LocationService.cs
+++ CashOverflowUz/Services/Foundetions/Locations/LocationService.cs
@@ -1,19 +1,44 @@
 ﻿using System.Threading.Tasks;
+using CashOverflowUz.Brokers.Loggings;
 using CashOverflowUz.Brokers.Storages;
 using CashOverflowUz.Models.Locations;
+using CashOverflowUz.Models.Locations.Exceptions;
 
 namespace CashOverflowUz.Services.Foundetions.Locations
 {
     public class LocationService : ILocationService
     {
-        public IStorageBroker StorageBroker;
+        private readonly IStorageBroker StorageBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public LocationService(IStorageBroker storageBroker, 
-            Brokers.Loggings.ILoggingBroker loggingBroker) =>
-            StorageBroker = storageBroker;
+        public LocationService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.StorageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
 
-        public async ValueTask<Location> AddLocationAsyncs(Location location) =>
-            await this.StorageBroker.InsertLocationAysnc(location);
+        public async ValueTask<Location> AddLocationAsyncs(Location location)
+        {
+            try
+            {
+            if (location is null)
+            {
+                throw new NullLocationException();
+            }
+            return await this.StorageBroker.InsertLocationAysnc(location);
 
+            }
+            catch (NullLocationException nullLocationException)
+            {
+                var locationValidationException =
+                     new LocationValidationException(nullLocationException);
+
+                this.loggingBroker.LogError(locationValidationException);
+                
+                throw locationValidationException;
+            }
+        }
     }
 }
