c3d00cd9d4c280d0eb3632d19013ab5f987a3c53
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-09 17:20:22 +0500

    CODERUB: Loggign Metod

---
+++ CashOverflowUz/Brokers/Loggigns/ILoggingBroker.cs
@@ -0,0 +1,16 @@
+﻿//-------------------------------------------------
+// Copyright (c) Coalition OF Good-Hearted Engineers 
+// Developet by CashOverflowUz Team
+//--------------------------------------------------
+
+
+using System;
+
+namespace CashOverflowUz.Brokers.Loggigns
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
