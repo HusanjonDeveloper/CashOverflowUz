0c6f2b0395ffd94a24a0a6538239fa9fdbc793d9
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-16 17:33:49 +0500

    BROKERS: Create DateTime  broker

---
+++ CashOverflowUz/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿//-------------------------------------------------
+// Copyright (c) Coalition OF Good-Hearted Engineers 
+// Developet by CashOverflowUz Team
+//--------------------------------------------------
+
+using System;
+
+namespace CashOverflowUz.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTimeOffset();
+    }
+}
